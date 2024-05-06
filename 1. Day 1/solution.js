4f46c3f6f2b43eb0fe05d2426b2c7e9e87f89ab3
diaryoc <diarycuriosity@gmail.com>
2024-05-06 08:15:20 +0700

    adding comments in the code for challenge day 1

--- 1. Day 1/solution.js
+++ 1. Day 1/solution.js
@@ -1,6 +1,10 @@
 function PairNumberForGivenSum(arrayOfNumber, target) {
+    // set up output in array
     var output = []
+
+    // first loop for get how many iteration needed base on length of array input
     for (let i = 0; i < arrayOfNumber.length; i++) {
+        // second loop for to get a pair that matches the desired sum result
         for (let j = i + 1; j < arrayOfNumber.length; j++) {
             if (i !== j) {
                 if (arrayOfNumber[i]+arrayOfNumber[j] === target) {
@@ -9,6 +13,8 @@ function PairNumberForGivenSum(arrayOfNumber, target) {
             }
         }
     }
+
+    // for print the result that we get
     if (output.length !== 0) {
         var stringOutput = ""
         for (let i = 0; i < output.length; i++) {
@@ -22,4 +28,5 @@ function PairNumberForGivenSum(arrayOfNumber, target) {
     };
     return "Pair not found"
 }
+// testing function
 console.log(PairNumberForGivenSum([2,3,1,5,6,7,4,9,8],10))
