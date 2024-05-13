312b91fc8d1d3de43edbe9b182601cbfee69358b
diaryoc <diarycuriosity@gmail.com>
2024-05-13 13:48:30 +0700

    5. Check Which Button is Pressed 6. Add Sounds to Button Clicks

--- 9. Day 9/solution/solution.js
+++ 9. Day 9/solution/solution.js
@@ -1,26 +1,54 @@
-let buttonColours = ["red", "blue", "green", "yellow"]
-let randomChosenColour =[]
-let gamePattern =[]
-function nextSequence() {
-    let randomNumber = Math.round(Math.random() * 3);
-    randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour);
+let buttonColor = ["red", "blue", "green", "yellow"]
+let gamePattern = []
+let userPattern = []
 
-    $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
-    let audio = new Audio("sounds/"+randomChosenColour+".mp3");
-    audio.play();
-    return gamePattern;
+$(document).keypress(function (event) {
+    nextSequence()
+})
+
+$(".btn").each(function () {
+    $(this).on("click", function () {
+        userPattern.push($(this).attr("id"));
+        if (gamePattern[userPattern.length - 1] === userPattern[userPattern.length - 1]) {
+            if (userPattern.length === gamePattern.length) {
+                setTimeout(function () {
+                    nextSequence()
+                },1000)
+            }
+        }else {
+            gameOver()
+        }
+        console.log(userPattern)
+        console.log(gamePattern)
+        playSound($(this).attr("id"))
+    })
+})
+
+function gameOver() {
+    gamePattern = []
+    playSound("wrong")
+    let body = $("body")
+    body.addClass("game-over")
+    setTimeout(function () {
+        body.removeClass("game-over")
+    },100)
 }
-$(document).on("keydown", (e)=>{
-    if (e.key === 'a'){
-        nextSequence();
-    }
-});
-
-$("div.btn").on("click", function (){
-    $(this).fadeOut(50).fadeIn(50);
-    let audio = new Audio("sounds/"+$(this).attr("id")+".mp3");
+function nextSequence() {
+    userPattern = []
+    let random = Math.floor(Math.random() * buttonColor.length);
+    let randomColor = buttonColor[random];
+    gamePattern.push(randomColor);
+    $("#level-title").text("Level "+gamePattern.length);
+    $("#"+randomColor).fadeOut(100).fadeIn(100);
+    playSound(randomColor)
+    console.log(userPattern)
+    console.log(gamePattern)
+}
+function playSound(name) {
+    let audio = new Audio("sounds/"+name+".mp3");
     audio.play();
-    nextSequence();
-    console.log(gamePattern);
-});
+}
+
+function animatedButton(){
+
+}
