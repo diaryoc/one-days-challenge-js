7fd412dc77d39b4b87319374c152a5492e0acc7e
diaryoc <diarycuriosity@gmail.com>
2024-05-13 11:01:22 +0700

    3. Create new pattern 4. Show the Sequence to the User with Animations and Sounds

--- 9. Day 9/solution/solution.js
+++ 9. Day 9/solution/solution.js
@@ -1,3 +1,26 @@
-$("h1").click(function (){
-    alert("Hello World!");
+let buttonColours = ["red", "blue", "green", "yellow"]
+let randomChosenColour =[]
+let gamePattern =[]
+function nextSequence() {
+    let randomNumber = Math.round(Math.random() * 3);
+    randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+    let audio = new Audio("sounds/"+randomChosenColour+".mp3");
+    audio.play();
+    return gamePattern;
+}
+$(document).on("keydown", (e)=>{
+    if (e.key === 'a'){
+        nextSequence();
+    }
+});
+
+$("div.btn").on("click", function (){
+    $(this).fadeOut(50).fadeIn(50);
+    let audio = new Audio("sounds/"+$(this).attr("id")+".mp3");
+    audio.play();
+    nextSequence();
+    console.log(gamePattern);
 });
