f91c651543b40c615194ab1b24aaa8c4ec936f7c
diaryoc <diarycuriosity@gmail.com>
2024-05-17 15:56:07 +0700

    add solution for challenge day 13

--- 13. To Do List/src/components/InputArea.jsx
+++ 13. To Do List/src/components/InputArea.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
-function InputArea() {
-  return (
+function InputArea(props) {
+    const [inputText, setInputText] = useState("");
+
+    function handleChange(event) {
+        const newValue = event.target.value;
+        setInputText(newValue);
+    }
+    return (
     <div className="form">
       <input onChange={handleChange} type="text" value={inputText} />
-      <button onClick={addItem}>
-        <span>Add</span>
+      <button onClick={()=> {
+          props.onAddItem(inputText);
+          setInputText("");
+      }}>
+        <span>Add Item</span>
       </button>
     </div>
   );
